3e3efe6f810389cec0dd56382108f486e599d9f9
vcdgibbs <vcdgibbs@gmail.com>
2021-02-10 19:58:47 -0300

    clear stuffs

--- utils/__init__.py
+++ utils/__init__.py
@@ -130,15 +130,3 @@ def printWarning(msg):
     print(fh + Colores.fg.yellow + " [WARNING] " + msg + Colores.reset)
     return 1
 
-
-
-''' 
-para una funcion de print mas elegante:
-
-quantity = 3
-itemno = 567
-price = 49.95
-myorder = "I want {} pieces of item {} for {} dollars."
-print(myorder.format(quantity, itemno, price))
-
-'''
