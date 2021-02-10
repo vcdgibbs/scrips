41ffb23eacfca48575906576ab18f5214fec4d9d
vcdgibbs <vcdgibbs@gmail.com>
2021-02-10 18:52:54 -0300

    Funcionalidad lista.

--- set-category.py
+++ set-category.py
@@ -20,20 +20,21 @@ Parameters:
    -sourceCSV <csv_file> : Indicate the path of a comma separated file including a list of VMs to modify. (1)(2)
                       The format of each line (with headers) is: 
 
-                      vm_name,category_name,category_value.
+                      vm_name,category_name,category_value
 
                       You can comment some VMs with '#' at the begining of the VM name, so those VMs will not be 
                       assigned to the Category:Value specified. e.g.
 
+                      vm_name,category_name,category_value
                       vm01, Environment, Production
                       #vm02, Evironment, Dev
 
                       In this example, vm01 will be assigned to Environment:Production, vm02 will be skipped.
                     
 
-   -category <category_name> : Name of the category to assign to the vm (which must already exists in Prism Central). 
+   -category <category_name> : Name of the category to assign to the vm (which must exists in Prism Central). 
 
-   -value <value_name>  : Name of the category value to assign to the vm (which must already exists in Prism Central).
+   -value <value_name>  : Value for the category to assign to the vm (which must also exists in Prism Central).
 
 Options:
 
@@ -50,8 +51,11 @@ Notes:
 Author: Victor D'Gibbs (victor.dgibbs@nutanix.com)
 Revision:  February 5, 2021
 
-(1) -vm and -sourceCSV are mutually exclusive, you one or the another one, but never toghether.
+(1) -vm and -sourceCSV are mutually exclusive, use either -vm or -sourceCSV but never toghether, -sourceCSV overides -vm
 (2) VM can't have duplicated names in the cluster, I can't decide the correct UUID.
+
+To do:
+- Log file
 '''
 
 
@@ -70,12 +74,9 @@ En un ambiente productivo esto no deberia estar.
 '''
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-pars=params2dict(sys.argv)
+# let's construct a DICT class from the arguments:
+pars = params2dict(sys.argv)
 
-##################### CONTROL #####################
-#print(pars)
-
-# Checking parameters and fill vars
 if "error" in pars:
     printError(pars['error'])
     exit(1)
@@ -119,7 +120,6 @@ if ("sourcecsv" in pars):
     printInfo("Using CSV file. Parameters -vm -category and -value will not be used.")
     # Let's fill a Dictionary field with the category and value associated to the VM.
     # Note: all initial whitespaces will be removed and in between will be replaced by a underscore ('_') character.
-
     with open(pars['sourcecsv']) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
@@ -145,11 +145,6 @@ if ("sourcecsv" in pars):
                 vmn = "vm" + str(line_count)
                 myListVMs[vmn] = myVarItem
                 line_count += 1
-        #print(f'\tProcessed {line_count} lines.')
-        # print(myListVMs)
-    #exit(0)
-
-
 else:
     if "vm" in pars and "category" in pars and "value" in pars:
         # código para dict de una sola VM
@@ -159,15 +154,12 @@ else:
             "value_name" : pars['value']
         }
         myListVMs = { "vm1" : myVarItem }
-        print(myListVMs)
     else:
         printError("Please provide the VM with Category and Value information or CSV file")
 
+# Enter password:
+Password = stdiomask.getpass()
 
-# Ingresar la password:
-# Password = stdiomask.getpass()
-# Por ahora dejo la password en duro
-Password = "bootCamp123!"
 
 # Variables
 API_Server_End_Point="https://" + PC_IP + ":9440/api/nutanix/v3/"
@@ -175,70 +167,82 @@ PrismCreds = {
     "User" : User,
     "Password" : Password
     }
-headers = {'ContentType':'application/json','cache-control': 'no-cache','Accept':'application/json'}
+#headers = {'ContentType':'application/json','cache-control': 'no-cache','Accept':'application/json'}
+headers = {'ContentType':'application/json','Accept':'application/json'}
 
+###################################################################################
 # API Calls to Prism Central
+# https://httpstatuses.com/ is a good page to know the HTTP status codes
+###################################################################################
+
 def Prism_API_Call(Method, URL, Credentials, Payload=None):
     printInfo("Making a " + Method + " call to " + URL)
     us=Credentials["User"]
     pw=Credentials["Password"]
    
-    # GET
+    # GET ##########################################################
     if Method == "GET":
         if Payload == None:
             r = requests.get(URL, auth=(us,pw), verify=False)
         else:
             r = requests.get(URL, auth=(us,pw), json=Payload, verify=False)
-        # print(r.status_code)
-        # Quiero entregar el status_code? del bloque siguiente:
-        if r.status_code==200:
-            printInfo("- all good, response code " + str(r.status_code) + " as expected :)")
+
+        if r.status_code==200 or r.status_code==202:
+            printInfo("All good, response code " + str(r.status_code) + " as expected :)")
         else:
-            printError("- Response code " + str(r.status_code))
-            #t=json.dumps(r.json())
-            #print (t)
+            printError("Response code " + str(r.status_code))
     
-    # POST
+    # POST #########################################################
     if Method == "POST":
         if Payload == None:
             r = requests.post(URL, auth=(us,pw), verify=False)
         else:
-            # Control:
-            # print(pl)
-            # print(type(pl))
             r = requests.post(URL, auth=(us,pw), json=Payload, verify=False)
-            # print(r.status_code)
-            # Quiero entregar el status_code?
-        if r.status_code==200:
-            printInfo("- all good, response code " + str(r.status_code) + " as expected :)")
-        else:
-            printError("- Response code " + str(r.status_code))
 
+        if r.status_code==200 or r.status_code==202:
+            printInfo("All good, response code " + str(r.status_code) + " as expected :)")
+        else:
+            printError("Response code " + str(r.status_code))
+
+    # PUT ##########################################################
+    if Method == "PUT":
+        if Payload == None:
+            r = requests.put(URL, auth=(us,pw), verify=False)
+        else:
+            r = requests.put(URL, auth=(us,pw), json=Payload, verify=False)
+
+        if r.status_code==200 or r.status_code==202:
+            printInfo("Response code " + str(r.status_code) + " as expected :)")
+        else:
+            printError("Response code " + str(r.status_code))
 
     # Python returns a "dict" with the JSON info, so returned value should be json.dumps('d) in order to use it as JSON in another API Call.
     # if returned JSON hasn't status code, I add it to the Dictionay.
     ret =  r.json()
     if "code" not in ret:
         ret["code"]=r.status_code
-    #print(type(ret))
     return ret
 
 for VMS in myListVMs:
-    #print(VMS)
-    #print(myListVMs[VMS]["vm_name"])
-    printInfo(Colores.reverse + "Let's process VM name \'" + myListVMs[VMS]["vm_name"] + "\'.")
+    
+    if "add" in pars.keys(): act="add"
+    else: act="remove"
+    Category = myListVMs[VMS]["category_name"]
+    Value = myListVMs[VMS]["value_name"]
+
+    printInfo(Colores.reverse + "Let's " + act + " the category " + Category+":"+Value+ " to  VM name \'" + myListVMs[VMS]["vm_name"] + "\'.     ")
     if myListVMs[VMS]["vm_name"][0] == "#":
         printInfo(myListVMs[VMS]["vm_name"].strip("#") + " marked with #, will do nothing with this one.")
         continue
+    
     ###############################################################
     # 1. Does Category exist?
     ###############################################################
-    Category = myListVMs[VMS]["category_name"]
-    Value = myListVMs[VMS]["value_name"]
+    
     printInfo("Checking whether " + Category + ":" + Value + " exists in " + PC_IP)
     comURL=API_Server_End_Point + "categories/" +  Category + "/" + Value
-    Method="GET"
-    resp = Prism_API_Call(Method,comURL,PrismCreds)
+    #Method="GET"
+    resp = Prism_API_Call("GET",comURL,PrismCreds)
     # resp is a 'dict' field    
     if "code" not in resp:
         resp["code"]=200
@@ -254,18 +258,17 @@ for VMS in myListVMs:
         exit(1)
 
     ###############################################################
-    # 2. Get VM details
+    # 2. Get VM details, UUID and prepare payload
     ###############################################################
    
-    # Let's get the VM UUID
-    # VM_UUID = {}
+    # Let's get the VM details
     comURL=API_Server_End_Point + "vms/list"
     flt="vm_name==" + myListVMs[VMS]["vm_name"]
     pl={
         "kind":"vm",
         "filter": flt
         }
-    print(type(pl))
+    
     try:
         vd = Prism_API_Call("POST",comURL,PrismCreds,pl)
         tvm = vd["metadata"]["total_matches"]
@@ -273,43 +276,78 @@ for VMS in myListVMs:
             printWarning("VM name \'" + myListVMs[VMS]["vm_name"] + "\' does not exist the cluster. (Names are case sensitive)")
             continue
         if tvm > 1:
+            # I can't decide which VM to assign the category to.
             printWarning("There are " + str(tvm) + " with VM with the same name \'" + myListVMs[VMS]["vm_name"] + "\'. Skiiping the VM")
             continue
     except:
             printError("Something get wrong, finishing")
             exit(1)
     
-    print(json.dumps(vd, indent=2))
-    print(vd.keys())
     t_vm=vd["entities"][0]
-    #print(Colores.fg.yellow)
-    #print(type(t_vm))
-    #print(json.dumps(t_vm, indent=2))
+    # deleteing old 'categories' key, I will use only 'categories_mapping' key:
+    t_vm["metadata"]["use_categories_mapping"]=True
+    t_vm["metadata"].pop("categories")
 
-    #t_vm=vd.get("entities")
-    #print("t_vm es una:")
-    #print(type(t_vm))
-    #print(len(t_vm))
-    # solo tiene 1 elemento la lista, debo procesarlo más
-    
-    print(Colores.reset)
-    print(Colores.fg.orange + "#################### asignar VM_Config ########################")
-    #print(t_vm["spec"])
-    VM_Config={"spec":t_vm["spec"]}
-    #print(type(VM_Config))
-    print(json.dumps(VM_Config, indent=2))
-    
-    print(Colores.reset + "#################### Obtener UUID ########################")
+    # Let's prepare the payload (JSON VM config) in VM_Config field:
+    VM_Config={
+        "spec" : t_vm["spec"], 
+        "api_version" : vd["api_version"],
+        "metadata": t_vm["metadata"]
+        }
+
+    # and let's get the VM UUID:
     VM_md={"metadata":t_vm["metadata"]}
-    #print(type(VM_md))
-    #print(json.dumps(VM_md, indent=2))
     VM_UUID=VM_md["metadata"]["uuid"]
-    print("UUID de la VM: "+ VM_UUID)
+    #print("UUID de la VM: "+ VM_UUID)
     
-'''
-## acli vm.get uuid -> JSON de la VM
-VM_UUID="a06a4857-0d0a-4ca0-a90c-aabcaf11ed7a"
-comURL=API_Server_End_Point + "vms/" + VM_UUID
-Method="GET"
-resp = Prism_API_Call(Method,comURL,PrismCreds)
-'''
+    ##########################################################################
+    # 3. Let's finish the payload with the addition or removal of categories
+    ##########################################################################
+    t1=VM_Config["metadata"]["categories_mapping"]
+
+    if "add" in pars.keys():
+        # 3.1 - Adding a category: 
+        
+        #print(t1.keys())
+        # does the VM have currently assigned the categry:value?
+        if Category in t1.keys() and Value in t1[Category]:
+            printWarning("VM " + myListVMs[VMS]["vm_name"] + "has already assigned the category " + Category + ":" + Value + ".  Doing nothing with this one.")
+            continue
+        
+        if Category in t1.keys():
+            VM_Config["metadata"]["categories_mapping"][Category].append(Value)
+        else:
+            # [Value] is for a list and not a single string.
+            VM_Config["metadata"]["categories_mapping"][Category]= [Value]
+    else:
+        # 3.2 - Removing the category
+
+        # VM should have assigned the Category:Value, if not, nothing to do:
+        if Category not in t1.keys():
+            printWarning("VM " + myListVMs[VMS]["vm_name"] + " does not have assigned the category \'" + Category + "\'.  Doing nothing with this one.")
+            continue
+
+        if Value not in VM_Config["metadata"]["categories_mapping"][Category]:
+            printWarning("VM " + myListVMs[VMS]["vm_name"] + " does not have assigned the value \'" + Value + " for category \'" + Category + "\'.  Doing nothing with this one.")
+            continue
+
+        if len(VM_Config["metadata"]["categories_mapping"][Category]) > 1:
+            VM_Config["metadata"]["categories_mapping"][Category].remove(Value)
+        else:
+            VM_Config["metadata"]["categories_mapping"].pop(Category)
+
+    ##########################################################################
+    # 4. Let's update the VM
+    ##########################################################################   
+    # Quieres ver el json de la configuración de la VM para el PUT?
+    #print(json.dumps(VM_Config, indent=2))
+    comURL = API_Server_End_Point + "vms/" + VM_UUID
+
+    try:
+        r1 = Prism_API_Call("PUT",comURL,PrismCreds,VM_Config)
+    except:
+        printError("Something get wrong.")
+
+# Everything runs as expected, good bye.
+printInfo(Colores.reverse + "I\'m done, cheers!                                   ")
+exit(0)
